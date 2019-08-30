9d8aa55a0477ecbe62a53f77dcb0f2bb6da1ba7e
Tyler Austen <TylerAusten@gmail.com>
2019-08-30 14:37:15 -0700

    #33 MapOfCollections - More test coverage.

---
+++ Ease.Util.Tests/Collections/Generic/MapOfHashSetsTests.cs
@@ -0,0 +1,17 @@
+﻿//
+// Copyright (c) 2019 Tyler Austen. See LICENSE file at top of repository for details.
+//
+
+using System.Collections.Generic;
+using Ease.Util.Collections.Generic;
+
+namespace Ease.Util.Tests.Collections.Generic
+{
+    public class MapOfHashSetsTests : MapOfConcreteCollectionsTestBase<MapOfHashSets<int, string>, int, HashSet<string>, string>
+    {
+        protected override int MainCollectionKey => 123;
+        protected override string MainCollectionValue => "Jiggy Wiggy";
+        protected override int SecondaryCollectionKey => 456;
+        protected override string SecondaryCollectionValue => "Wagga Wagga";
+    }
+}
